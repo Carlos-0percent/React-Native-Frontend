244c17bcc2eae462ebdb3ef51ed648680a827b3f
Rohit <rohit@Rohits-MacBook-Pro.local>
2021-03-25 22:56:29 +0530

    android & ios folders removed

--- ios/Pods/Target Support Files/Pods-ZeroPercent-ZeroPercentTests/Pods-ZeroPercent-ZeroPercentTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZeroPercent_ZeroPercentTests : NSObject
-@end
-@implementation PodsDummy_Pods_ZeroPercent_ZeroPercentTests
-@end
--- ios/Pods/Target Support Files/Pods-ZeroPercent-tvOSTests/Pods-ZeroPercent-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZeroPercent_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ZeroPercent_tvOSTests
-@end
